cebabf93171155d5e816c106b52893c979e443b3
Russell <miller.time.baby@gmail.com>
2022-03-18 08:53:25 -0700

    add type hints for babynames

--- exercises/babynames/babynames.py
+++ exercises/babynames/babynames.py
@@ -34,7 +34,7 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
-def extract_names(filename):
+def extract_names(filename: str) -> list[str]:
   """
   Given a file name for baby.html, returns a list starting with the year string
   followed by the name-rank strings in alphabetical order.
@@ -44,7 +44,7 @@ def extract_names(filename):
   return
 
 
-def main():
+def main() -> None:
   # This command-line parsing code is provided.
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
@@ -63,6 +63,6 @@ def main():
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
-  
+
 if __name__ == '__main__':
   main()
--- exercises/babynames/solution/babynames.py
+++ exercises/babynames/solution/babynames.py
@@ -34,7 +34,7 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
-def extract_names(filename):
+def extract_names(filename: str) -> list[str]:
   """
   Given a file name for baby.html, returns a list starting with the year string
   followed by the name-rank strings in alphabetical order.
@@ -94,7 +94,7 @@ def extract_names(filename):
   # LAB(end solution)
 
 
-def main():
+def main() -> None:
   # This command-line parsing code is provided.
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
